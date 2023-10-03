43091abc4d90ddccef63c26f46d4df197b8738b8
新藤真子 <144088336+shindoumako@users.noreply.github.com>
2023-10-03 05:36:34 +0000

    自然について

---
+++ components/Navbar.vue
@@ -0,0 +1,8 @@
+<template>
+  <nav>
+    <ul>
+      <li><NuxtLink to="/">First</NuxtLink></li>
+      <li><NuxtLink to="/second">Second</NuxtLink></li>
+    </ul>
+  </nav>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
     app: {
+        pageTransition:{name:'page',mode:'out-in'},
         // baseURL: '/Nuxt3Bootstrap5AppDemo/', // baseURL: '/<repository>/'
         head: {
             meta: [
