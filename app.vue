fa08018ac10544116bf7be616c9de00968b72d69
新藤真子 <144088336+shindoumako@users.noreply.github.com>
2023-10-03 05:44:33 +0000

    自然災害

--- app.vue
+++ app.vue
@@ -6,14 +6,3 @@
 </div>
 </template>
 
-<style>
-.page-enter-active,
-.page-enter-active{
-transition: all 0.4s;
-}
-.page-enter-from,
-.page-leave-to{
-opacity:0;
-filter:blur(1rem);
-}
-</style>
--- pages/second.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div>
-    <h1>Second Page</h1>
-  </div>
-</template>
