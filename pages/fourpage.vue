93b37a456031ba4f59db9867a955b870ee1f7219
新藤真子 <144088336+shindoumako@users.noreply.github.com>
2023-10-31 05:48:02 +0000

    hennkou

---
+++ pages/fourpage.vue
@@ -0,0 +1,36 @@
+<script lang="ts" setup>
+useHead({
+  title: 'Newt・Nuxtフォーム',
+  meta: [
+    { name: 'description', content: 'NewtとNuxtを利用した問い合わせフォームです' }
+  ]
+})
+</script>
+
+<template>
+  <div class='container'>
+    <h2>form</h2>
+    <form action="https://Naturaldisasters.form.newt.so/v1/NMBwNi3OC" method="post">
+      <p><label for="name">匿名</label></p>
+      <input id="name" name="name">
+     <p><label for="email">Email</label></p>
+      <input id="email" name="email" type="email">
+      <p><label for="message">コメント</label></p>
+      <textarea id="message" name="message" />
+      <p><button type="submit">
+        送信
+      </button></p>
+    </form>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  font-family: arial;
+  font-size: 20px;
+  margin: 150px;
+  width: 350px;
+  height: 400px;
+  outline: dashed 1px black;
+}
+</style>
