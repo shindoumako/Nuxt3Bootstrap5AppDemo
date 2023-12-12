904b7455f031490b99df2a3656eb573801386020
新藤真子 <144088336+shindoumako@users.noreply.github.com>
2023-12-12 04:30:31 +0000

    改善

--- pages/fourpage.vue
+++ pages/fourpage.vue
@@ -1,12 +1,18 @@
-
+<template>
+<div class="form">
+<h1>お問い合わせ</h1>
+</div>
+</template>
 
 <style scoped>
-.container {
-  font-family: arial;
-  font-size: 20px;
-  margin: 150px;
-  width: 350px;
-  height: 400px;
-  outline: dashed 1px black;
-}
+.form
+  h1{
+  position: relative;/*相対位置*/
+  padding: 0.5em 0.5em 0.5em 1.5em;/*アイコン分のスペース*/
+  line-height: 1.4;/*行高*/
+  color: #4e8851;/*文字色*/
+  border-top: dotted 1px gray;
+  border-bottom: dotted 1px gray;
+  background: #fafaf3;
+  }
 </style>
